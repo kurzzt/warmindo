72f402a471c44a41eca76378b17919410d57c017
Apasha99 <elisakds88@gmail.com>
2023-12-17 15:28:10 +0700

    zzz

--- lib/src/screens/sign_in.dart
+++ lib/src/screens/sign_in.dart
@@ -1,8 +1,7 @@
-// Copyright 2021, the Flutter project authors. Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
 import 'package:flutter/material.dart';
+import 'package:supabase/supabase.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:warung_sample/src/auth.dart';
 
 class Credentials {
   final String username;
@@ -16,8 +15,8 @@ class SignInScreen extends StatefulWidget {
 
   const SignInScreen({
     required this.onSignIn,
-    super.key,
-  });
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<SignInScreen> createState() => _SignInScreenState();
@@ -27,43 +26,84 @@ class _SignInScreenState extends State<SignInScreen> {
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
 
+  Future<void> _signInWithEmailAndPassword() async {
+    final username = _usernameController.text.trim();
+    final password = _passwordController.text.trim();
+
+    if (username.isEmpty || password.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: const Text('Please enter username and password'),
+          backgroundColor: Theme.of(context).colorScheme.error,
+        ),
+      );
+      return; // Stop the sign-in process if username or password is empty
+    }
+
+    try {
+      // Panggil metode signIn dari WarungAuth
+      final auth = WarungAuth.of(context);
+      final success = await auth.signIn(username, password);
+
+      if (success) {
+        widget.onSignIn(Credentials(username, password));
+        // Redirect to the next screen or perform necessary actions upon successful sign-in
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: const Text('Invalid credentials'),
+            backgroundColor: Theme.of(context).colorScheme.error,
+          ),
+        );
+      }
+    } catch (error) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: const Text('Error occurred, please retry.'),
+          backgroundColor: Theme.of(context).colorScheme.error,
+        ),
+      );
+    }
+  }
+
+
   @override
-  Widget build(BuildContext context) => Scaffold(
-        body: Center(
-          child: Card(
-            child: Container(
-              constraints: BoxConstraints.loose(const Size(600, 600)),
-              padding: const EdgeInsets.all(8),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text('Sign in',
-                      style: Theme.of(context).textTheme.headlineMedium),
-                  TextField(
-                    decoration: const InputDecoration(labelText: 'Username'),
-                    controller: _usernameController,
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Card(
+          child: Container(
+            constraints: BoxConstraints.loose(const Size(600, 600)),
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'Sign in',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                TextField(
+                  decoration: const InputDecoration(labelText: 'Username'),
+                  controller: _usernameController,
+                ),
+                TextField(
+                  decoration: const InputDecoration(labelText: 'Password'),
+                  obscureText: true,
+                  controller: _passwordController,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: TextButton(
+                    onPressed: _signInWithEmailAndPassword,
+                    child: const Text('Sign in'),
                   ),
-                  TextField(
-                    decoration: const InputDecoration(labelText: 'Password'),
-                    obscureText: true,
-                    controller: _passwordController,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: TextButton(
-                      onPressed: () async {
-                        widget.onSignIn(Credentials(
-                            _usernameController.value.text,
-                            _passwordController.value.text));
-                      },
-                      child: const Text('Sign in'),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
-      );
+      ),
+    );
+  }
 }
